7e28075da2d07b27535c6c9b7d1b418ec53dece1
mingyubeom <alsrb524@naver.com>
2020-12-02 21:15:15 +0900

    Initial Commit

---
+++ Target Support Files/Pods-iglistkityogi-iglistkityogiUITests/Pods-iglistkityogi-iglistkityogiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iglistkityogi_iglistkityogiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iglistkityogi_iglistkityogiUITests
+@end
---
+++ Target Support Files/Pods-iglistkityogi/Pods-iglistkityogi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iglistkityogi : NSObject
+@end
+@implementation PodsDummy_Pods_iglistkityogi
+@end
